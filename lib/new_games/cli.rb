f7c6a41722fe90ac3189506dee4cde99867771e4
benser1 <briganenser28@gmail.com>
2016-02-21 10:24:56 -0700

    fully functioning gem

--- lib/new_games/cli.rb
+++ lib/new_games/cli.rb
@@ -3,7 +3,47 @@ require 'pry'
 class NewGames::CLI 
 
   def call
-    Game.doc
+    Game.scrape_all
+    list 
+    menu
+  end
+
+  def list 
+    puts ""
+    puts "************* Xbox One Upcoming Games ****************"
+    puts "" 
+    Game.all.each.with_index(1) do |game, i| 
+      puts "#{i}. #{game.title}"
+    end
+  end ## list end
+
+  def menu
+    input = nil 
+    while input != "exit"
+      puts "----------------------------------------------"
+      puts "Which game would you like to know more about?"
+      puts "" 
+      puts "Type the number that matches the game"
+      puts ""
+      puts "Type list to see the list again"
+      puts "Type exit to end the program"
+      puts "----------------------------------------------"
+      input = gets.strip
+      if input == "list"
+        list
+      elsif input.to_i.between?(1, Game.all.size)
+        game = Game.find(input)
+        puts "   Name: #{game.title}"
+        puts "   Date: #{game.date}"
+        puts "   Price: #{game.price}"
+        puts "   Publisher: #{game.publisher}"
+      elsif !input.to_i.between?(1, Game.all.size)
+          puts "Invalid number. Please type a valid number"
+      else
+        "goodbye"
+      end
+    end
+    puts "Come back to see a list of upcoming Xbox One games"
   end
 
 
--- lib/new_games/game.rb
+++ lib/new_games/game.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 class Game 
 
+  attr_accessor :title, :price, :publisher, :date
+
   @@all = []
 
   def self.all 
@@ -13,28 +15,66 @@ class Game
   end
 
   def self.find(id) 
-    self.all[id.to_i -1]
+    self.all[id.to_i - 1]
   end
 
   def self.doc
     @@doc ||= Nokogiri::HTML(open("http://www.gamestop.com/browse/games/xbox-one?nav=28-xu0,13ffff2418-1e0-5"))
-    binding.pry
   end
 
   def self.product_title
-    @@title ||= doc.search(".ats-product-title").collect{|e| e.text}
+    @@title ||= doc.search(".ats-product-title a").collect{|e| e.text}
   end
 
-  def self.price
+  def self.game_price
     @@price ||= doc.search(".ats-product-price").collect{|e| e.text}
   end
 
-  def self.publisher
+  def self.game_publisher
     @@publisher ||= doc.search(".ats-product-publisher").collect{|e| e.text}
   end
 
+  def self.release_date
+    @@date ||= doc.search(".ats-product-infoList li:first-child").collect{|e| e.text}
+  end
+
+  def self.scrape_all
+    (0..product_title.size-1).to_a.each do |i|
+      title = product_title[i]
+      price = game_price[i]
+      publisher = game_publisher[i]
+      date = release_date[i]
+
+      game = Game.new 
+      game.title = title 
+      game.price = price 
+      game.publisher = publisher
+      game.date = date 
+      game.save 
+    end
+  end
 
 
 
 
 end ## class end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
